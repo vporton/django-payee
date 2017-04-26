c276f05498e1b1437b3abcbbd17155dedfaa19e6
Victor Porton <porton@narod.ru>
2017-04-26 15:57:57 +0300

    bug fix

--- payments/payments_base/models.py
+++ payments/payments_base/models.py
@@ -4,6 +4,7 @@ from dateutil.relativedelta import relativedelta
 import logging
 from django.apps import apps
 from django.urls import reverse
+import django.db
 from django.db import models
 from django.db.models import F
 from django.core.exceptions import ObjectDoesNotExist
@@ -372,7 +373,8 @@ class Subscription(models.Model):
     # duplicates email in Payment
     email = models.EmailField(null=True)  # DalPay requires to notify the customer 10 days before every payment
 
-    @transaction.atomic
+    # `transaction` identifier is overridden above
+    @django.db.transaction.atomic
     def force_cancel(self):
         if self.subscription_reference:
             klass = model_from_ref(self.transaction.processor.api)
