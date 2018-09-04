828ea79e9f505f99f1439d687e365698ec30ed06
Victor Porton <porton@narod.ru>
2018-09-05 01:19:05 +0300

    bug fix

--- debits/debits_test/callbacks.py
+++ debits/debits_test/callbacks.py
@@ -14,8 +14,8 @@ class MyPayPalIPN(PayPalIPN):
             self.do_purchase(item)
 
     def do_purchase(self, item):
-        organization = item.testsubscriptionitem.purchase.for_organization  # FIXME: item has no purchase
+        organization = item.testsubscriptionitem.for_purchase.for_organization  # FIXME: item has no purchase
         if organization is None:
             return
-        organization.purchase = item.testsubscriptionitem.purchase
+        organization.purchase = item.testsubscriptionitem.for_purchase
         organization.save()
