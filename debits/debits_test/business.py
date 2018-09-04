97a90254abfe9ce608eda3d64112f08e1ae9bb47
Victor Porton <porton@narod.ru>
2018-09-05 01:26:32 +0300

    bug fix

--- debits/debits_test/business.py
+++ debits/debits_test/business.py
@@ -6,17 +6,19 @@ from .models import Organization, Purchase, PricingPlan, TestSubscriptionItem
 @transaction.atomic
 def create_organization(name, pricing_plan_id, trial_months):
     plan = PricingPlan.objects.get(pk=pricing_plan_id)
-    item = TestSubscriptionItem(product=plan.product,
+    purchase = Purchase(plan=plan)
+    purchase.save()
+    item = TestSubscriptionItem(purchase=purchase,
+                                product=plan.product,
                                 currency=plan.currency,
                                 price=plan.price,
                                 payment_period_unit=Period.UNIT_MONTHS,
                                 payment_period_count=1,
                                 trial_period_unit=Period.UNIT_MONTHS,
                                 trial_period_count=trial_months)
-    purchase = Purchase(plan=plan, item=item)
-    item.purchase = purchase
     if trial_months:
         item.start_trial()
     item.save()
-    purchase.save()
+    purchase.item = item
+    purchase.save()  # second time
     return Organization.objects.create(name=name, purchase=purchase)
