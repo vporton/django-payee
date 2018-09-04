87c4a0f4895b3b2e90ac070b72329d51d9dafb59
Victor Porton <porton@narod.ru>
2018-09-05 01:32:33 +0300

    bug fix

--- debits/debits_test/models.py
+++ debits/debits_test/models.py
@@ -23,7 +23,7 @@ class TestSubscriptionItem(SubscriptionItem):
 
 class Purchase(models.Model):
     plan = models.ForeignKey(PricingPlan, on_delete=models.CASCADE)
-    item = models.OneToOneField(SubscriptionItem, on_delete=models.CASCADE, related_name='for_purchase')
+    item = models.OneToOneField(SubscriptionItem, on_delete=models.CASCADE, related_name='for_purchase', null=True)
     # Don't mess .for_organization with .organization!
     for_organization = models.ForeignKey('Organization', null=True, related_name='for_purchase', on_delete=models.CASCADE)
 
