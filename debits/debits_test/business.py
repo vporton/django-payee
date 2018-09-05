373a07adb1f2c2fa1e31f883f0912aba15c74897
Victor Porton <porton@narod.ru>
2018-09-05 03:16:22 +0300

    bug fix

--- debits/debits_test/business.py
+++ debits/debits_test/business.py
@@ -6,7 +6,8 @@ from .models import Organization, Purchase, PricingPlan
 @transaction.atomic
 def create_organization(name, pricing_plan_id, trial_months):
     plan = PricingPlan.objects.get(pk=pricing_plan_id)
-    item = Purchase(product=plan.product,
+    item = Purchase(plan=plan,
+                    product=plan.product,
                     currency=plan.currency,
                     price=plan.price,
                     payment_period_unit=Period.UNIT_MONTHS,
