9fba155cc40990ccfcac0b2b99b2f7babd9ea0b9
Victor Porton <porton@narod.ru>
2018-09-30 14:37:47 +0300

    bug fix

--- debits/debits_test/business.py
+++ debits/debits_test/business.py
@@ -12,13 +12,13 @@ def create_organization(name, pricing_plan_id, trial_months):
 
     It also associates a :class:`~debits.debits_test.models.MyPurchase` with it."""
     plan = PricingPlan.objects.get(pk=pricing_plan_id)
-    item = debits.debits_base.models.SubscriptionItem(product=plan.product,
-                                                      currency=plan.currency,
-                                                      price=plan.price,
-                                                      payment_period_unit=Period.UNIT_MONTHS,
-                                                      payment_period_count=1,
-                                                      trial_period_unit=Period.UNIT_MONTHS,
-                                                      trial_period_count=trial_months)
+    item = debits.debits_base.models.SubscriptionItem.objects.create(product=plan.product,
+                                                                     currency=plan.currency,
+                                                                     price=plan.price,
+                                                                     payment_period_unit=Period.UNIT_MONTHS,
+                                                                     payment_period_count=1,
+                                                                     trial_period_unit=Period.UNIT_MONTHS,
+                                                                     trial_period_count=trial_months)
     purchase = MyPurchase(item=item, plan=plan)
     if trial_months:
         purchase.start_trial()
