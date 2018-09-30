43cc56345e4a99fa28624a0679c466541aaf7898
Victor Porton <porton@narod.ru>
2018-09-30 21:40:51 +0300

    FIXME comment

--- debits/debits_test/views.py
+++ debits/debits_test/views.py
@@ -42,7 +42,7 @@ def do_organization_payment_view(request, purchase, organization):
                    'active': purchase.is_active(),
                    'blocked': purchase.blocked,
                    'manual_mode': not purchase.subscribed,
-                   'processor_name': purchase.payment.transaction.processor.name if purchase.payment else None,
+                   'processor_name': purchase.payment.transaction.processor.name if purchase.payment else None,  # FIXME: Does not work for subscription with delayed first payment
                    # only for automatic recurring payment
                    'plan': purchase.plan.name,
                    'trial': purchase.trial,
