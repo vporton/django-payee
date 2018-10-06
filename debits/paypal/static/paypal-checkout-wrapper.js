a1cd54f802a25dafcabb7738b470e9813a27a01a
Victor Porton <porton@narod.ru>
2018-10-06 17:00:56 +0300

    debits_render_paypal_button() JS function

---
+++ debits/paypal/static/paypal-checkout-wrapper.js
@@ -0,0 +1,22 @@
+function debits_render_paypal_button(html_id, mode, create_cb, execute_cb, hash, options, confirmation) {
+  var all_options = {
+    env: mode, //'sandbox' or 'production'
+    payment: function(data, actions) {
+      return actions.request.post(create_cb, hash)
+        .then(function(res) {
+          return res.id;
+        });
+    },
+    onAuthorize: function(data, actions) {
+      return actions.request.post(execute_cb, {
+        paymentID: data.paymentID,
+        payerID:   data.payerID
+      })
+        .then(function(res) {
+          confirmation();
+        });
+    }
+  };
+  for(var attrname in options) { all_options[attrname] = options[attrname]; }
+  paypal.Button.render(all_options, '#'+html_id);
+}
